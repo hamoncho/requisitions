7d924e2b53ae240e6578575b1db0648662a30b73
hamoncho <hamsvidal@gmail.com>
2025-05-14 16:37:54 -0600

    use component admin-table in view objective index

--- resources/views/objective/index.blade.php
+++ resources/views/objective/index.blade.php
@@ -4,61 +4,11 @@
             {{ __('Objectives') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white shadow sm:rounded-lg px-1 py-6 sm:p-6">
-                <div class="flex justify-between items-center mb-4">
-                    <h3 class="text-lg font-medium text-gray-900">List of Items</h3>
-                    <a href="{{ route('objective.create') }}"
-                        class="inline-flex items-center px-4 py-2 bg-blue-950 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:bg-blue-900 active:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                        + Create Item
-                    </a>
-                </div>
-
-                @if ($objectives->isEmpty())
-                    <p class="text-gray-500">No budget items found.</p>
-                @else
-                    <div class="overflow-x-auto">
-                        <table class="w-full divide-y divide-gray-200">
-                            <thead class="bg-gray-100">
-                                <tr>
-                                    <th
-                                        class="px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Index + Name
-                                    </th>
-                                    <th
-                                        class="px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Description
-                                    </th>
-                                    <th
-                                        class="px-1 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Actions
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach ($objectives as $item)
-                                    <tr>
-                                        <td class="px-1 py-4 whitespace-wrap font-semibold text-gray-700">
-                                            {{ $item->index . ' ' . $item->name }}
-                                        </td>
-                                        <td class="px-1 py-4 whitespace-wrap text-gray-900">
-                                            {{ $item->description }}
-                                        </td>
-                                        <td>
-                                            <a href="{{ route('objective.edit', $item) }}"
-                                                class="inline-flex items-center px-4 py-2 bg-blue-950 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:bg-blue-900 active:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                Edit
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                @endif
-            </div>
-        </div>
-    </div>
+    <x-admin-table
+    :tableInfo="$objectives"
+    :columns="['name', 'description']"
+    :routeCreate="route('objective.create')"
+    routeEdit="objective.edit">
+    {{__('List of items')}}
+    </x-admin-table>
 </x-app-layout>

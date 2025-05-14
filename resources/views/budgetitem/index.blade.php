52f5fd0d8d310d693d8eb82ea76d70c8763d60a7
hamoncho <hamsvidal@gmail.com>
2025-05-14 17:19:58 -0600

    use component admin-table in view budget items

--- resources/views/budgetitem/index.blade.php
+++ resources/views/budgetitem/index.blade.php
@@ -4,101 +4,11 @@
             {{ __('Budget Items') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white shadow sm:rounded-lg p-6">
-                <div class="flex justify-between items-center mb-4">
-                    <h3 class="text-lg font-medium text-gray-900">List of Items</h3>
-                    <a href="{{ route('budget_item.create') }}"
-                        class="inline-flex items-center px-4 py-2 bg-blue-950 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:bg-blue-900 active:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                        + Create Item
-                    </a>
-                </div>
-
-                @if ($budgetItems->isEmpty())
-                    <p class="text-gray-500">No budget items found.</p>
-                @else
-                    <div class="overflow-x-auto">
-                        <table class="min-w-full divide-y divide-gray-200">
-                            <thead class="bg-gray-50">
-                                <tr>
-                                    <th
-                                        class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Code</th>
-                                    <th
-                                        class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Name</th>
-                                    <th
-                                        class="hidden sm:block px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach ($budgetItems as $item)
-                                    <tr>
-                                        <td class="px-2  whitespace-nowrap font-semibold text-gray-700">
-                                            {{ $item->full_code }}</td>
-                                        <td class="whitespace-wrap text-gray-900">
-                                            <details class="bg-gray-100">
-                                                <summary
-                                                    class="py-2 pl-2 cursor-pointer bg-blue-950 font-bold text-gray-200">
-                                                    {{ $item->name }}
-                                                </summary>
-                                                <ul class="pl-4">
-                                                    <li class="py-2">
-                                                        <strong>Category: </strong>
-                                                        @if (is_null($item->generalBudgetItem))
-                                                            <p class="text-gray-500">No budget items found.</p>
-                                                        @else
-                                                            {{ $item->generalBudgetItem->name }}
-                                                        @endif
-                                                    </li>
-                                                    <li class="py-2 sm:hidden">
-                                                        <a href="{{ route('budget_item.edit', $item) }}"
-                                                            class="inline-flex items-center px-4 py-2 bg-blue-950 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:bg-blue-900 active:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                            Edit
-                                                        </a>
-
-                                                        <form method="POST"
-                                                            action="{{ route('budget_item.destroy', $item) }}"
-                                                            class="inline-block"
-                                                            onsubmit="return confirm('Are you sure you want to delete this item?');">
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            <button type="submit"
-                                                                class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-500 active:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                                Delete
-                                                            </button>
-                                                        </form>
-                                                    </li>
-                                                </ul>
-                                            </details>
-                                        </td>
-                                        <td class="hidden sm:block px-2 whitespace-nowrap space-x-2">
-                                            <a href="{{ route('budget_item.edit', $item) }}"
-                                                class="inline-flex items-center px-4 py-2 bg-blue-950 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-900 focus:bg-blue-900 active:bg-blue-950 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                Edit
-                                            </a>
-
-                                            <form method="POST" action="{{ route('budget_item.destroy', $item) }}"
-                                                class="inline-block"
-                                                onsubmit="return confirm('Are you sure you want to delete this item?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit"
-                                                    class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 focus:bg-red-500 active:bg-red-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                                                    Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                @endif
-            </div>
-        </div>
-    </div>
+    <x-admin-table
+    :tableInfo="$budgetItems"
+    :columns="['full_code','name']"
+    :routeCreate="route('budget_item.create')"
+    routeEdit="budget_item.edit">
+        {{__('List of items')}}
+    </x-admin-table>
 </x-app-layout>

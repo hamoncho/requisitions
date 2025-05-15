050178df92e374d61aa0a2b2ba0887f3ebc9e5cd
hamoncho <hamsvidal@gmail.com>
2025-05-15 08:01:16 -0600

    use component secondary-link-button in view edit objective

--- resources/views/objective/edit.blade.php
+++ resources/views/objective/edit.blade.php
@@ -20,13 +20,12 @@
                                     {{ __('Update the details of this objective.') }}
                                 </p>
                             </div>
-                            <a href="{{ route('objective.index') }}"
-                                class="h-fit inline-flex items-center px-4 py-2 bg-gray-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-500 active:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                            <x-secondary-link-button href="{{ route('objective.index') }}">
                                 {{ __('Back') }}
-                            </a>
+                            </x-secondary-link-button>
                         </header>
 
-                        <form method="post" action="{{ route('objective.update', $objective->id) }}"
+                        <form method="post" action="{{ route('objective.update', $objective) }}"
                             class="mt-6 space-y-6">
                             @csrf
                             @method('put')

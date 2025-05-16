ff9e98cc38b53b7000cafddfdbb477806d8e37e0
hamoncho <hamsvidal@gmail.com>
2025-05-15 22:07:50 -0600

    fix(web): typo in view create a new general budget
    change string "Budgeti" to "Budget".

--- resources/views/generalbudgetitem/create.blade.php
+++ resources/views/generalbudgetitem/create.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            General Budgeti Items
+            General Budget Items
         </h2>
     </x-slot>
 

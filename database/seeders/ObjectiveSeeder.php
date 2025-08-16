d22c8396c58b2c08eae8345e1c5a775546e69f63
hamoncho <hamsvidal@gmail.com>
2025-08-16 15:22:45 -0600

    fix(database): fix typo in ObjectiveSeeder
    change objectivo for objetivo

--- database/seeders/ObjectiveSeeder.php
+++ database/seeders/ObjectiveSeeder.php
@@ -38,39 +38,39 @@ class ObjectiveSeeder extends Seeder
 
         Objective::create([
             'index' => '1',
-            'name' => 'Objectivo 1',
+            'name' => 'Objetivo 1',
             'description' => 'Fortalecer la calidad de  la oferta educativa'
         ]);
 
         Objective::create([
             'index' => '2',
-            'name' => 'Objectivo 2',
+            'name' => 'Objetivo 2',
             'description' => ' Ampliar la cobertura con un enfoque de equidad y justicia social'
         ]);
 
         Objective::create([
             'index' => '3',
-            'name' => 'Objectivo 3',
+            'name' => 'Objetivo 3',
             'description' => 'Impulsar la formación integral de los estudiantes para contribuir al desarrollo de todas sus potencialidades'
         ]);
 
         Objective::create([
             'index' => '4',
-            'name' => 'Objectivo 4',
+            'name' => 'Objetivo 4',
             'description' => 'Robustecer la investigación científica, el desarrollo tecnológico y la innovación a fin de contribuir al desarrollo del país y a mejorar el bienestar de la sociedad.'
         ]);
 
 
         Objective::create([
             'index' => '5',
-            'name' => 'Objectivo 5',
+            'name' => 'Objetivo 5',
             'description' => 'Consolidar la vinculación con los sectores público, social y privado.'
         ]);
 
 
         Objective::create([
             'index' => '6',
-            'name' => 'Objectivo 6',
+            'name' => 'Objetivo 6',
             'description' => 'Modernizar la gestión institucional con transparencia y rendición de cuentas'
         ]);
 
